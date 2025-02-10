9fe69c0020de47f30b28ae57086a19633eb62b7a
prasanna1292 <147026767+prasanna1292@users.noreply.github.com>
2025-02-10 21:35:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def display_data():
     """
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)  # Debug mode OFF for production
